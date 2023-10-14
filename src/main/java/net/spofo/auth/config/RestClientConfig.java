9faaae3661a3b776e547f905208bf2139880cb90
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-10-14 14:55:48 +0900

    RestClient에 interceptor 적용

--- src/main/java/net/spofo/auth/config/RestClientConfig.java
+++ src/main/java/net/spofo/auth/config/RestClientConfig.java
@@ -1,14 +1,29 @@
 package net.spofo.auth.config;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestClientConfig {
+
     @Bean
     public RestClient restClient() {
-        return RestClient.create(new RestTemplate());
+        return RestClient.builder()
+                .requestInterceptor(clientHttpRequestInterceptor())
+                .build();
+    }
+
+    @Bean
+    public ClientHttpRequestInterceptor clientHttpRequestInterceptor() {
+        return (request, body, execution) -> {
+            HttpHeaders headers = request.getHeaders();
+            headers.setContentType(APPLICATION_JSON);
+            return execution.execute(request, body);
+        };
     }
 }
