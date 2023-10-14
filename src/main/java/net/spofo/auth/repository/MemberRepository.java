26e3a5652c9574d4931673c4f9b83a1ff4909a67
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-10-14 15:34:44 +0900

    쿼리 메소드 키워드 제시

--- src/main/java/net/spofo/auth/repository/MemberRepository.java
+++ src/main/java/net/spofo/auth/repository/MemberRepository.java
@@ -6,5 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    /*
+        참고
+        이 메서드가 왜 동작하는지 공부하면 좋을 것 같아요!
+        (키워드 : Spring Data JPA 쿼리 메소드)
+     */
     Optional<Member> findBySocialId(String socialId);
 }
